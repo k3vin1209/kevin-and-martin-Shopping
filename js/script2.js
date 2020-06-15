7a76bb63b7ac798077b004412969d09405576045
k3vin1209 <kevin.stamenkovski@gmail.com>
2020-06-15 17:55:19 +0200

    commit this site of K3VIN AND MARTIN

---
+++ js/script2.js
@@ -0,0 +1,36 @@
+
+function myFunction1() {
+	document.getElementById("myDropdown1").classList.toggle("show");
+	document.getElementById("myDropdown1").addEventListener("mouseout", myFunction1);	
+	document.getElementById("myDropdown1").addEventListener("mousein", myFunction1);		
+}
+
+function myFunction2() {
+	document.getElementById("myDropdown2").classList.toggle("show");
+	document.getElementById("myDropdown2").addEventListener("mouseout", myFunction2);	
+}
+
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+  document.getElementById("myDropdown3").addEventListener("mouseout", myFunction3);
+}
+
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+  document.getElementById("myDropdown4").addEventListener("mouseout", myFunction4);
+}
+
+function myFunction5() {
+  document.getElementById("myDropdown5").classList.toggle("show");
+  document.getElementById("myDropdown5").addEventListener("mouseout", myFunction5);
+}
+
+function myFunction6() {
+  document.getElementById("myDropdown6").classList.toggle("show");
+  document.getElementById("myDropdown6").addEventListener("mouseout", myFunction6);
+}
+
+function myFunction7() {
+  document.getElementById("myDropdown7").classList.toggle("show");
+}
